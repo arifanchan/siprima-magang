25cc0465b92e04323c4f0bfddab06f0a52f1e7f0
Arifa Chan <19215277@bsi.ac.id>
2025-06-23 18:57:35 +0700

    Add FilePond dependencies for enhanced file handling; implement delete action in EditFinalAssessment page; update package files accordingly

--- app/Filament/Resources/FinalAssessmentResource/Pages/EditFinalAssessment.php
+++ app/Filament/Resources/FinalAssessmentResource/Pages/EditFinalAssessment.php
@@ -2,10 +2,18 @@
 namespace App\Filament\Resources\FinalAssessmentResource\Pages;
 
 use App\Filament\Resources\FinalAssessmentResource;
+use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditFinalAssessment extends EditRecord
 {
     protected static string $resource = FinalAssessmentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
 }
 
